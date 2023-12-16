515333b396b29e018550da0ffc770a30cd62ab9d
Brennan Hughes <brennanhughes@icloud.com>
2023-12-15 19:51:59 -0500

    moved headline_scraper.py

--- src/headline_scraper.py
+++ src/headline_scraper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-def scrape_headlines(site, keyword):
+def scrape_headlines(site, keywords):
     url = site['url']
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -9,17 +9,18 @@ def scrape_headlines(site, keyword):
     headlines = []
     
     for headline in soup.find_all(site['headline_div']):
-        if keyword.lower() in headline.text.lower():
-            date = headline.find_next(site['date_div']).text
-            headlines.append({'headline': headline.text, 'date': date})
+        for keyword in keywords:
+            if keyword.lower() in headline.text.lower():
+                date = headline.find_next(site['date_div']).text
+                headlines.append({'headline': headline.text, 'date': date})
     
     return headlines
 
 
 def main():
     site = {'url': 'https://www.bbc.com/news', 'headline_div': 'h2', 'date_div': 'span'}
-    keyword = 'Ukraine'
-    headlines = scrape_headlines(site, keyword)
+    keywords = ['Google', 'GOOG', 'Alphabet', 'GOOGL'];
+    headlines = scrape_headlines(site, keywords)
     print(headlines)
 
 if __name__ == '__main__':
