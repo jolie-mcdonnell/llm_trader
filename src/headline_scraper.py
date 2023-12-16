8f842eb19308117aa8c6e66fa4886a57cfc09db6
Brennan Hughes <brennanhughes@icloud.com>
2023-12-15 21:59:57 -0500

    added google news and washington post

--- src/headline_scraper.py
+++ src/headline_scraper.py
@@ -12,31 +12,54 @@ def scrape_headlines(site, keywords):
             headline_text = headline.text.strip()
             for keyword in keywords:
                 if keyword.lower() in headline_text.lower():
-                    date = headline.find_next(attrs=site['date_attrs'])
-                    if date:
-                        date_text = date.text.strip()
-                    else:
+                    if site['date_attrs'] is None:
                         date_text = 'Date not found'
-                    description = headline.find_next(attrs=site['description_attrs'])
-                    if description:
-                        description_text = description.text.strip()
                     else:
+                        date = headline.find_next(attrs=site['date_attrs'])
+                        if date:
+                            date_text = date.text.strip()
+                        else:
+                            date_text = 'Date not found'
+                    if site['description_attrs'] is None:
                         description_text = 'Description not found'
+                    else:
+                        description = headline.find_next(attrs=site['description_attrs'])
+                        if description:
+                            description_text = description.text.strip()
+                        else:
+                            description_text = 'Description not found'
                     matching_headlines.append({'headline':headline_text,
                                             'description':description_text,
                                             'date':date_text,
-                                            'url':url,
+                                            'source':site['name'],
                                             'business':keywords[0]})
                     break
         return matching_headlines
     else:
         print("Failed to retrieve the webpage. Status code:", response.status_code)
 
-nyt = {'url': 'https://www.nytimes.com/section/business', 'headline_attrs': {'class':'css-1kv6qi e15t083i0'}, 'description_attrs': {'class':'css-1pga48a e15t083i1'}, 'date_attrs': {'data-testid':'todays-date'}}
-bbc = {'url': 'https://www.bbc.com/business', 'headline_attrs': {'data-testid': 'card-headline'}, 'description_attrs': {'data-testid': 'card-description'}, 'date_attrs': {'data-testid': 'card-metadata-lastupdated'}}
+nyt = {'name': 'New York Times', 'url': 'https://www.nytimes.com/section/business', 'headline_attrs': {'class':'css-1kv6qi e15t083i0'}, 'description_attrs': {'class':'css-1pga48a e15t083i1'}, 'date_attrs': {'data-testid':'todays-date'}}
+bbc = {'name': 'BBC', 'url': 'https://www.bbc.com/business', 'headline_attrs': {'data-testid': 'card-headline'}, 'description_attrs': {'data-testid': 'card-description'}, 'date_attrs': {'data-testid': 'card-metadata-lastupdated'}}
+google = {'name': 'Google News', 'url': 'https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen', 'headline_attrs': {'class': 'gPFEn'}, 'description_attrs': None, 'date_attrs': {'class': 'hvbAAd'}}
+washpo = {'name': 'Washington Post', 'url': 'https://www.washingtonpost.com/business/', 'headline_attrs': {'data-pb-local-content-field':"web_headline"}, 'description_attrs': None, 'date_attrs': None}
+# wall street journal
+# bloomberg
+# financial times
+# reuters
+# cnbc
+# fortune
+# business insider
+# the economist
+# barrons
+# marketwatch
+# cnn
+# fox news
+# msnbc
+# politico
 
-sites = [nyt, bbc]
-companies = [['Netflix', 'NFLX'],
+sites = [nyt, bbc, google, washpo]
+companies = [
+             ['Netflix', 'NFLX'],
              ['Activision', 'ATVI'],
              ['Apple', 'AAPL', 'iPhone', 'iPad', 'iMac'],
              ['Microsoft', 'MSFT'],
