cce07a90975534d1e65b0553e7e01980f8571998
Brennan Hughes <brennanhughes@icloud.com>
2023-12-15 20:38:07 -0500

    added nytimes/business

--- src/headline_scraper.py
+++ src/headline_scraper.py
@@ -4,24 +4,24 @@ from bs4 import BeautifulSoup
 def scrape_headlines(site, keywords):
     url = site['url']
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    headlines = []
-    
-    for headline in soup.find_all(site['headline_div']):
+    soup = BeautifulSoup(response.content, 'html.parser')
+    headlines = soup.find_all('h3')
+    matching_headlines = []
+    for headline in headlines:
         for keyword in keywords:
             if keyword.lower() in headline.text.lower():
-                date = headline.find_next(site['date_div']).text
-                headlines.append({'headline': headline.text, 'date': date})
-    
-    return headlines
+                # date = headline.find_next('span', attrs={'data-testid': "todays-date"}).text
+                matching_headlines.append({'headline':headline.text, 'url':url, 'business':keywords[0]})
+                break
+    return matching_headlines
 
+nyt_business = {'url': 'https://www.nytimes.com/section/business', 'headline_tag': 'h3'}
+bbc = {'url': 'https://www.bbc.com/news', 'headline_tag': 'h2'}
 
-def main():
-    site = {'url': 'https://www.bbc.com/news', 'headline_div': 'h2', 'date_div': 'span'}
-    keywords = ['Google', 'GOOG', 'Alphabet', 'GOOGL'];
-    headlines = scrape_headlines(site, keywords)
-    print(headlines)
+print(scrape_headlines(nyt_business, ['Netflix', 'NFLX']))
+print(scrape_headlines(nyt_business, ['Activision', 'ATVI']))
+print(scrape_headlines(nyt_business, ['Apple', 'AAPL']))
 
-if __name__ == '__main__':
-    main()
+print(scrape_headlines(bbc, ['Netflix', 'NFLX']))
+print(scrape_headlines(bbc, ['Activision', 'ATVI']))
+print(scrape_headlines(bbc, ['Apple', 'AAPL']))
