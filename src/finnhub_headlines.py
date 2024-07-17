803e43ccf4a2e9db56ab64df85eafc9d58a58467
Jolie McDonnell <joliemcdonnell@MacBook-Air-9.home>
2024-07-16 21:37:15 -0400

    added 2 more finnhub api keys

--- src/finnhub_headlines.py
+++ src/finnhub_headlines.py
@@ -7,11 +7,15 @@ import datetime as dt
 
 FINNHUB_API_KEY = os.getenv("FINNHUB_API_KEY")
 FINNHUB_API_KEY_2 = os.getenv("FINNHUB_API_KEY_2")
+FINNHUB_API_KEY_3 = os.getenv("FINNHUB_API_KEY_3")
+FINNHUB_API_KEY_4 = os.getenv("FINNHUB_API_KEY_4")
 
 
 # Initialize the Finnhub clients with your API key
 finnhub_client = finnhub.Client(api_key=FINNHUB_API_KEY)
 finnhub_client_2 = finnhub.Client(api_key=FINNHUB_API_KEY_2)
+finnhub_client_3 = finnhub.Client(api_key=FINNHUB_API_KEY_3)
+finnhub_client_4 = finnhub.Client(api_key=FINNHUB_API_KEY_4)
 
 today = dt.datetime.today().date()
 COUNTER = 0
@@ -37,8 +41,34 @@ def get_headlines(ticker):
             print(COUNTER)
             return headlines
         except:
-            time.sleep(5)
-            get_headlines(ticker)
+            try:
+                # Fetch news headlines for the ticker from Finnhub API
+                news = finnhub_client_3.company_news(
+                    symbol=ticker, _from=today, to=today
+                )
+                # Extract headlines from the response
+                headlines = [
+                    (article["headline"], article["datetime"]) for article in news
+                ]
+                COUNTER += 1
+                print(COUNTER)
+                return headlines
+            except:
+                try:
+                    # Fetch news headlines for the ticker from Finnhub API
+                    news = finnhub_client_4.company_news(
+                        symbol=ticker, _from=today, to=today
+                    )
+                    # Extract headlines from the response
+                    headlines = [
+                        (article["headline"], article["datetime"]) for article in news
+                    ]
+                    COUNTER += 1
+                    print(COUNTER)
+                    return headlines
+                except:
+                    time.sleep(5)
+                    get_headlines(ticker)
 
 
 if __name__ == "__main__":
